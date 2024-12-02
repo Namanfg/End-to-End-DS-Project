d718e8d480af829c71c4203b6c85e10e0706003c
Naman Sharma <namansharmaves@gmail.com>
2024-12-03 00:34:51 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from src.endtoendproject.components.data_ingestion import DataIngestion
 from src.endtoendproject.components.data_ingestion import DataIngestionConfig
 from src.endtoendproject.components.data_transformation import DataTransformationConfig
 from src.endtoendproject.components.data_transformation import DataTransformation
+from src.endtoendproject.components.model_trainer import ModelTrainerConfig
+from src.endtoendproject.components.model_trainer import ModelTrainer
 import sys
 
 
@@ -17,8 +19,12 @@ if __name__=="__main__":
 
         # data_transformation_config=data_transformation_config()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
         
+        model_trainer_config = ModelTrainerConfig()
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+    
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
